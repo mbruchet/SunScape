560b838a6a76b53e2130b07e8eefa4f204f7d0f3
Michel Bruchet <mbruchet@hsdyn.com>
2024-03-02 01:25:39 +0100

    Use multicontainer deployment for asp.net core blazor web app

---
+++ SunScape/Services/DockerEnvironmentExtension.cs
@@ -0,0 +1,23 @@
+﻿namespace SunScape.Services;
+
+public static class DockerEnvironmentExtension
+{
+    public static bool IsDocker(this IHostEnvironment env)
+    {
+        var dockerRuningInContainer = Environment.GetEnvironmentVariable("DOTNET_RUNNING_IN_CONTAINER");
+
+        if(!string.IsNullOrEmpty(dockerRuningInContainer))
+        {
+            return bool.Parse(dockerRuningInContainer);
+        }
+
+        var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+
+        if(!string.IsNullOrEmpty(environmentName))
+        {
+            return environmentName.Equals("Docker", StringComparison.OrdinalIgnoreCase);
+        }
+
+        return false;
+    }
+}
